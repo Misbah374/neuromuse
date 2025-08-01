28fdf8b70536eaad68e7924c95892f6d690494e9
Misbah374 <misbahuttariq52@gmail.com>
2025-08-01 23:35:26 +0530

    set up basic layout and added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = () => {
+    return(
+        <header>
+            <h1>NEUROMUSE</h1>
+            <p>Let's listen to your mood</p>
+        </header>
+    );
+};
+export default Header;
