2e1f355e17732864a4ac1a760d0ec99596fe3839
Misbah374 <misbahuttariq52@gmail.com>
2025-08-20 17:31:00 +0530

    Final and very basic minimal viable project

--- src/components/MoodSelector.jsx
+++ src/components/MoodSelector.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import TomeGenerator from "./MusicPlayer";
 
 const MoodSelector = ( {frequency, onFrequencyChange} ) => {
-    const [selectedMood, setSelectedMood] = useState(null);
-    const moods = ["happy","sad","angry","calm","neutral"];
+    /*const [selectedMood, setSelectedMood] = useState(null);
+    const moods = ["happy","sad","angry","calm","neutral"];*/
     const handleChange = (e) => {
         const newValue = e.target.value;
         if(typeof onFrequencyChange === 'function'){
@@ -13,16 +13,16 @@ const MoodSelector = ( {frequency, onFrequencyChange} ) => {
 
     return(
         <div>
-            <div className="card-container" id="mainContent">
+            {/*<div className="card-container" id="mainContent">
                 {moods.map((mood) => (
-                    /*<div key={mood} className={`rect-card ${selectedMood === mood ? "selected" : ""}`} onClick={*/
+                    /*<div key={mood} className={`rect-card ${selectedMood === mood ? "selected" : ""}`} onClick={
                     <div key={mood} className={`rect-card`} onClick={
                         () => setSelectedMood(mood)}>
                             {mood}
                     </div>
                 ))}
             </div>
-            <p>Want a custom frequency sound:<br></br>
+            <p>Want a custom frequency sound:<br></br>*/}<p>
             <input type="number" value={frequency} onChange={handleChange} min="20" max="20000" placeholder="Enter frequency in Hz"></input>
             </p>
             {/*<hr color="#000000" width="90%"></hr><br></br>*/}
