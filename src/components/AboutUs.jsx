44534b1f540a478dc2520dad1f44a1ea0a6d2854
Misbah374 <misbahuttariq52@gmail.com>
2025-08-20 17:14:50 +0530

    Almost everything is set.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import myImage from "../assets/moodplot.jpg";
 
 const AboutUs = () => {
     return(
         <div className="about" style={{marginBottom:'20px'}}>
             <h2>About Neuromuse</h2>
             <p>
-                This website is inspired by a research paper "A Preliminary Investigation on Frequency Dependent Cues for Human Emotions". The developer tries
-                to implement the conclusions of the paper in the website so that people can use the research in thier daily life.
+                This project is a technical implementation based on the research paper <a href="https://www.mdpi.com/2624-599X/4/2/28">"A Preliminary 
+                Investigation on Frequency Dependent Cues for Human Emotions"</a> authored by Manish Kumar, Thushara D. Abhayapala, and Prasanga Samarasinghe.
             </p>
             <p>
-                This website may be used by writers who have to write a particular emotion or genre. Graphic designer can also use this to do thier work based 
-                on required theme.
+                The project visually demonstrates the relationships between pure tone frequencies and perceived human emotions, using data findings from 
+                the paper. It is intended for educational purposes only and does not claim to be a definitive or scientifically validated tool for emotion 
+                recognition.
             </p>
+            <img src={myImage} style={{width:'100%'}}></img>
             <p>
-                Neuromuse helps in setting the mood, but if overused, the user may not relying on their natural emotions. It can reduce emotional authenticity
-                and lead the user to avoid their real feelings, which can be harmful to long term creativity.
+                Above image is only for educational purpose. All rigths are reserved by the authors of research paper (links of the actual paper is given above).
             </p>
         </div>
     );
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,54 +1,58 @@
-import react, {useState} from "react";
+import React, {useState, useRef} from "react";
 import MoodSelector from "./MoodSelector";
 
 const TomeGenerator = () =>{
     const [isPlaying, setIsPlaying] = useState(false);
     const [freq, setFreq] = useState(440);
+    const audioContextRef = useRef(null);
+    const oscillatorRef = useRef(null);
     const handleFrequencyChange = (newFreq) => {
         setFreq(newFreq);
+        if(oscillatorRef.current){
+            oscillatorRef.current.frequency.value = newFreq;        // update live if playing
+        }
     }
 
     const toggleTone = () => {
-        if(isPlaying){
-            console.log("Tone is playing currently...");
-            return;
+        if(!isPlaying){
+            setIsPlaying(true);
+            // create a new AudioContext and oscillator for each tone
+            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            const oscillator = audioContext.createOscillator();
+            const gainNode = audioContext.createGain();
+
+            // set the desired frequency and waveform
+            oscillator.frequency.value = freq;      // 440 Hz default
+            oscillator.type = 'sine'    // Pure tone
+
+
+            //connect the nodes: oscillator -> gain -> speakers
+            oscillator.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+
+            //start the oscillator immediately
+            oscillator.start();
+
+            // save references
+            audioContextRef.current = audioContext;
+            oscillatorRef.current = oscillator;
         }
-        setIsPlaying(true);
-        // create a new AudioContext and oscillator for each tone
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const oscillator = audioContext.createOscillator();
-        const gainNode = audioContext.createGain();
-
-        // set the desired frequency and waveform
-        oscillator.frequency.value = freq;      // 440 Hz default
-        oscillator.type = 'sine'    // Pure tone
-
-        //set a gain value to control the volume
-        gainNode.gain.value = 1;
-
-        //connect the nodes: oscillator -> gain -> speakers
-        oscillator.connect(gainNode);
-        gainNode.connect(audioContext.destination);
-
-        //start the oscillator immediately
-        oscillator.start();
-
-        // stop the oscillator after a set duration like 10 seconds
-        const stopTime = audioContext.currentTime + 10;
-        oscillator.stop(stopTime);
-
-        //After the given duration, set the state back to not playing
-        setTimeout(() => {
+        else{
+            // Stopping current sound
+            oscillatorRef.current.stop();
+            oscillatorRef.current.disconnect();
+            oscillatorRef.current = null;
+            audioContextRef.current.close();
+            audioContextRef.current = null;    
             setIsPlaying(false);
-            audioContext.close();
-        }, 10000);
+        }
     };
 
     return(
         <div>
             <MoodSelector frequency={freq} onFrequencyChange={handleFrequencyChange}/>
-            <button onClick={toggleTone} >    {/*disabled={isPlaying}*/}
-                {isPlaying? 'Playing...':`Play ${freq}`}
+            <button onClick={toggleTone} >    
+                {isPlaying? 'Stop':`Play ${freq}`}
             </button>
             <br></br><br></br>
         </div>
